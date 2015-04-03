7def15b0ab274a5b4c2d34da28f10a2aaca4770e
Angelo Hochmann <angelohochmann@Angelo-Hochmanns-iMac.local>
2015-04-03 20:30:07 +0200

    changes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-FooCoin (FOO)
+SlvCoin (SLC)
 ===========
 
-[![Build Status](https://travis-ci.org/RazorLove/foocoin.png?branch=master)](https://travis-ci.org/RazorLove/foocoin)
+[![Build Status](https://travis-ci.org/RazorLove/slvcoin.png?branch=master)](https://travis-ci.org/RazorLove/slvcoin)
 
 
 Scrypt Hashcash PoW Template
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -45,13 +45,13 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("FooCoin");
-    case mBTC: return QString("milliFooCoin (1 / 1,000)");
-    case uBTC: return QString("microFooCoin (1 / 1,000,000)");
+    case BTC: return QString("SlvCoin");
+    case mBTC: return QString("milliSlvCoin (1 / 1,000)");
+    case uBTC: return QString("microSlvCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
-//a single unit (.00000001) of FooCoin is called a "wander."
+//a single unit (.00000001) of SlvCoin is called a "wander."
 qint64 BitcoinUnits::factor(int unit)
 {
     switch(unit)
